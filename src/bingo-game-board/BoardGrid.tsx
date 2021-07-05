ed496809c61a6991ee41699ddb4b4f4644e56715
njrey <nolan.reynolds.2@gmail.com>
2021-07-05 11:46:43 -0400

    feat(BoardGrid): Refactor BoardGrid as a functional component.

--- src/bingo-game-board/BoardGrid.tsx
+++ src/bingo-game-board/BoardGrid.tsx
@@ -1,4 +1,3 @@
-import { Component } from "react";
 import { Square } from "./Square";
 import "./BoardGrid.css";
 
@@ -7,57 +6,40 @@ interface SquareTile {
   textContent: string;
 }
 
-export class BoardGrid extends Component<{ tileList: Array<string> }, {}> {
-  constructor(props: any) {
-    super(props);
-    this.state = { tileList: [] };
-    this.getRandomInt = this.getRandomInt.bind(this);
-  }
+function shuffle(arr: Array<any>) {
+  let x = arr.length;
+  let j = 0;
+  let temp = [];
 
-  getRandomInt(max: number) {
-    return Math.floor(Math.random() * Math.floor(max));
+  while (x--) {
+    j = Math.floor(Math.random() * (x + 1));
+    temp = arr[x];
+    arr[x] = arr[j];
+    arr[j] = temp;
   }
-  shuffle(arr: Array<any>) {
-    let x = arr.length;
-    let j = 0;
-    let temp = [];
-
-    while (x--) {
-      j = Math.floor(Math.random() * (x + 1));
-      temp = arr[x];
-      arr[x] = arr[j];
-      arr[j] = temp;
-    }
-    let freeSpaceIndex = arr.indexOf("free space");
-    if (freeSpaceIndex >= 0) {
-      let temp = arr[12];
-      arr[12] = "free space";
-      arr[freeSpaceIndex] = temp;
-    }
-    return arr;
+  let freeSpaceIndex = arr.indexOf("free space");
+  if (freeSpaceIndex >= 0) {
+    let temp = arr[12];
+    arr[12] = "free space";
+    arr[freeSpaceIndex] = temp;
   }
-
-  create1dBoardArray(arr: string[]): Array<SquareTile> {
-    let board: Array<SquareTile> = [];
-    arr.forEach((item, index) =>
-      board.push({ textContent: item, boardIndex: index })
-    );
-    return board;
-  }
-  render() {
-    let gameBoard: Array<SquareTile> = this.create1dBoardArray(
-      this.shuffle(this.props.tileList)
-    );
-
-    return (
-      <div>
-        <h1>Bingo</h1>
-        <div className="grid-board">
-          {gameBoard.map((square) => (
-            <Square key={square.boardIndex} tileText={square.textContent} />
-          ))}
-        </div>
-      </div>
-    );
-  }
+  return arr;
 }
+
+export const BoardGrid = ({ tileList }: { tileList: string[] }) => {
+  let gameBoard: Array<SquareTile> = shuffle(tileList).map((item, index) => ({
+    textContent: item,
+    boardIndex: index,
+  }));
+
+  return (
+    <div>
+      <h1>Bingo</h1>
+      <div className="grid-board">
+        {gameBoard.map((square) => (
+          <Square key={square.boardIndex} tileText={square.textContent} />
+        ))}
+      </div>
+    </div>
+  );
+};
